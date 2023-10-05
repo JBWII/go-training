748cfe055a6a9c3f2b2c52db0dc5b008a0c270f1
JBWII <brad@bradwelborn.com>
2023-10-05 14:51:53 -0400

    Added comments

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
+// Package gotrain is a training package that helps teach how to set up GO packages.
+//
+// It includes one function Add that adds two integers together.
 package gotrain
 
+// Add preforms [additon] on two integers and returns the result.
+//
+// [additon]: https://www.mathsisfun.com/numbers/addition.html
 func Add(i1, i2 int) int {
 	return i1 + i2
 }
